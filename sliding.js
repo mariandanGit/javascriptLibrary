488ce39c11525402e32cc01cf8d08f173675eb61
Marian Dan <danmarian713@gmail.com>
2022-01-26 20:29:53 +0200

    Still got work to do

--- sliding.js
+++ sliding.js
@@ -1,20 +1,69 @@
 const slideMenu = document.getElementById("side-form");
+const actualForm = document.getElementById("form");
 const overlay = document.getElementById("overlay");
+const cancelButton = document.getElementById("cancel-button");
 
-function slide(){
+var flag = false;
+
+function slideHorizontally(){
    slideMenu.classList.toggle("active");
 
    if(slideMenu.classList.contains("active")){
-        slideMenu.style.width = "100%";
-        //overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+
+     slideMenu.removeEventListener("click", slideHorizontally);
+     checkWindowSize();
+
+     slideMenu.style.width = "100%";
+     slideMenu.style.height = "100vh";
+     overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     actualForm.style.display = "flex";
    }
    else{
-        slideMenu.style.width = "80px";
-        overlay.style.left = "80px";
-        overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
-        slideMenu.style.flexDirection = "row";
+
+     slideMenu.style.width = "80px";
+     slideMenu.style.height = "100vh";
+     overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+     slideMenu.style.flexDirection = "row";
+     actualForm.style.display = "none";
    }
 }
 
-slideMenu.addEventListener("mouseenter", slide);
-slideMenu.addEventListener("mouseleave", slide);
+function slideVertically(){
+     slideMenu.classList.toggle("active");
+  
+     if(slideMenu.classList.contains("active")){
+          slideMenu.style.height = "100%";
+          slideMenu.style.width = "100%";
+          actualForm.style.display = "flex";
+     }
+     else{
+          slideMenu.style.width = "100%";
+          slideMenu.style.height = "80px";
+          actualForm.style.display = "none";
+     }
+}
+
+function checkWindowSize(){
+     if(window.innerWidth <= 800){
+
+          slideMenu.style.width = "100%";
+          slideMenu.style.height = "80px";
+          actualForm.style.display = "none";
+          overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+
+          slideMenu.removeEventListener("click", slideHorizontally);
+          slideMenu.addEventListener("click", slideVertically);
+     }
+     else{
+
+          slideMenu.style.width = "80px";
+          slideMenu.style.height = "100vh";
+          actualForm.style.display = "none";
+
+          slideMenu.removeEventListener("click", slideVertically);
+          slideMenu.addEventListener("click", slideHorizontally);
+     }
+}
+
+checkWindowSize();
+window.addEventListener("resize", checkWindowSize);
